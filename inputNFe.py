8008e746d7eee8bd694f6fb123830dc54c0981e1
FTAlves <ftalves@poli.ufrj.br>
2018-02-26 02:24:07 -0300

    Remove external dependency
    Change parser to built-in XML parser. More robust and keeps dependencies easy to maintain.

--- inputNFe.py
+++ inputNFe.py
@@ -1,5 +1,5 @@
-#import xml.etree.ElementTree as eTree
-import xmltodict
+import xml.etree.ElementTree as eTree
+#import xmltodict
 from src.inputNFe.cleanNFe import *
 
 #def tags(tree,tagDict):
@@ -10,18 +10,27 @@ from src.inputNFe.cleanNFe import *
 #        #fullTag = nodeName + nodeAttrib
 #        tagDict[nodeName] = {'value':nodeText,'attrib':nodeAttrib}
 
-#xml_tree = eTree.parse(filename)
-#xml_root = xml_tree.getroot()
-
 filename = 'data/xml_test.xml'
 
 filename = cleanNFe(filename)
 fileclean = open(filename)
 
-dict_tree = xmltodict.parse(fileclean.read())
+xml_tree = eTree.parse(filename)
+xml_root = xml_tree.getroot()
+#dict_tree = xmltodict.parse(fileclean.read())
+
+#prodEAN = []
+#
+#NFeID = dict_tree['nfeProc']['protNFe']['infProt']['chNFe']
+#for iprod in range(len(dict_tree['nfeProc']['NFe']['infNFe']['det'])):
+#    prodEAN.append(dict_tree['nfeProc']['NFe']['infNFe']['det'][iprod]['prod']['cEAN'])
 
 prodEAN = []
 
-NFeID = dict_tree['nfeProc']['protNFe']['infProt']['chNFe']
-for iprod in range(len(dict_tree['nfeProc']['NFe']['infNFe']['det'])):
-    prodEAN.append(dict_tree['nfeProc']['NFe']['infNFe']['det'][iprod]['prod']['cEAN'])
+for prot in xml_root.iter('infProt'):
+    NFeID = prot.find('chNFe').text
+
+for product in xml_root.iter('prod'):
+    prodEAN.append(product.find('cEAN').text)
+
+fileclean.close()
