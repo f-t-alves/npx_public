2f13620b14d0f42d64b0a51ec37775ad0f5543c1
FTAlves <ftalves@poli.ufrj.br>
2018-02-27 18:12:49 -0300

    Add NFe control check
    Checks for duplicate NFe entry and cancels operation. Stores XML in database as a string

--- src/inputNFe/inputNFe2DB.py
+++ src/inputNFe/inputNFe2DB.py
@@ -2,16 +2,41 @@ import traceback
 
 def inputNFe2DB(inDict,db):
     cursor = db.cursor()
-    inList = []
+    stockList = []
+
     length = len(inDict['prodEAN'])
+    NFeID = str(inDict['NFeID'])
+    NFeFile = str(inDict['NFeFile'])
+
+    nfeTuple = (NFeID,NFeFile)
     for i in range(length):
-        inList.append((inDict['prodEAN'][i],inDict['prodQuantity'][i]))
+        stockList.append((inDict['prodEAN'][i],inDict['prodQuantity'][i]))
+
+    flagDupe = checkDuplicateNFe(NFeID,cursor)
+    if flagDupe==True:
+        print('Duplicate NFe, operation cancelled')
+        return
+
     try:
+        cursor.execute('''
+        INSERT INTO NFeControl(NFeID,XMLFile)
+           VALUES(?,?)''', nfeTuple)
+
         cursor.executemany('''
-        INSERT OR IGNORE INTO StockControl(EAN,Quantity)
-           VALUES(?,?)''', inList)
+        INSERT INTO StockControl(EAN,Quantity)
+           VALUES(?,?)''', stockList)
         db.commit()
     except:
         db.rollback()
         print('inputNFe failed')
         traceback.print_exc()
+
+def checkDuplicateNFe(checkID,cursor):
+    cursor.execute('''SELECT NFeID FROM NFeControl''')
+    checkList = cursor.fetchall()
+    flag = False
+    for cell in checkList:
+        flag = checkID in cell
+        if flag==True:
+            break
+    return flag
