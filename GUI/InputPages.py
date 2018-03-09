fdb66987b79d18d6ac1c698a7289f69565f86a15
Guilherme <gcaneca@poli.ufrj.br>
2018-03-08 23:40:23 -0300

    GUI V1.2

--- GUI/InputPages.py
+++ GUI/InputPages.py
@@ -5,7 +5,7 @@ from tkinter import ttk
 
 LARGE_FONT= ("Verdana", 12)
 NORM_FONT= ("Verdana", 10)
-SMALL_FONT= ("Verdana", 8)
+SMALL_FONT= ("Verdana", 7)
 
 def popupmsg(msg):
     popup =tk.Tk()
@@ -21,35 +21,96 @@ def popupmsg(msg):
     popup.resizable(width=False, height=False)
     popup.mainloop()
 
-class StrInputBoxes(tk.Frame):
 
-    def __init__(self,InputNames,EntryNumbers,ColumnNumber,parent):
+
+class FloatInputBoxes(tk.Frame):
+
+    def __init__(self,InputNames,EntryNumbers,parent):
         tk.Frame.__init__(self,parent)
         self.InputNames = InputNames
         self.EntryNumbers = EntryNumbers
-        self.ColumnNumber = ColumnNumber
         self.entries = []
 
-        self.create_entry_widgets(self.EntryNumbers,self.ColumnNumber,self.InputNames,
+        self.create_entry_widgets(self.EntryNumbers,self.InputNames,
         self.entries)
 
-    def create_entry_widgets(self,EntryNumbers,ColumnNumber,InputNames,entries):
+    def create_entry_widgets(self,EntryNumbers,InputNames,entries):
 
         self.ctrl1 = int(EntryNumbers)
-        self.ctrl2 = int(ColumnNumber)
         self.List = InputNames
 
         for i in range(self.ctrl1):
 
-            self.text = tk.Label(self,text = self.List[i],)
+            self.text = tk.Label(self,text = self.List[i],font=SMALL_FONT)
 
-            self.text.grid(row=i, column = self.ctrl2, sticky = 'E',pady=2)
+            self.text.grid(row=i, column = 0, sticky = 'E',pady=2)
             self.text.grid_columnconfigure(1,weight=1)
 
         for i in range(self.ctrl1):
 
             self.entr = tk.Entry(self,bg='azure')
-            self.entr.grid(row=i, column = self.ctrl2+1, sticky = 'E',pady=2)
+            self.entr.grid(row=i, column = 1, sticky = 'E',pady=2)
+            self.entries.append(self.entr)
+
+
+
+class IntInputBoxes(tk.Frame):
+
+    def __init__(self,InputNames,EntryNumbers,parent):
+        tk.Frame.__init__(self,parent)
+        self.InputNames = InputNames
+        self.EntryNumbers = EntryNumbers
+        self.entries = []
+
+        self.create_entry_widgets(self.EntryNumbers,self.InputNames,
+        self.entries)
+
+    def create_entry_widgets(self,EntryNumbers,InputNames,entries):
+
+        self.ctrl1 = int(EntryNumbers)
+        self.List = InputNames
+
+        for i in range(self.ctrl1):
+
+            self.text = tk.Label(self,text = self.List[i],font=SMALL_FONT)
+
+            self.text.grid(row=i, column = 0, sticky = 'E',pady=2)
+            self.text.grid_columnconfigure(1,weight=1)
+
+        for i in range(self.ctrl1):
+
+            self.entr = tk.Entry(self,bg='azure')
+            self.entr.grid(row=i, column = 1, sticky = 'E',pady=2)
+            self.entries.append(self.entr)
+
+
+class StrInputBoxes(tk.Frame):
+
+    def __init__(self,InputNames,EntryNumbers,parent):
+        tk.Frame.__init__(self,parent)
+        self.InputNames = InputNames
+        self.EntryNumbers = EntryNumbers
+        self.entries = []
+
+        self.create_entry_widgets(self.EntryNumbers,self.InputNames,
+        self.entries)
+
+    def create_entry_widgets(self,EntryNumbers,InputNames,entries):
+
+        self.ctrl1 = int(EntryNumbers)
+        self.List = InputNames
+
+        for i in range(self.ctrl1):
+
+            self.text = tk.Label(self,text = self.List[i],font=SMALL_FONT)
+
+            self.text.grid(row=i, column = 0, sticky = 'E',pady=2)
+            self.text.grid_columnconfigure(1,weight=1)
+
+        for i in range(self.ctrl1):
+
+            self.entr = tk.Entry(self,bg='azure')
+            self.entr.grid(row=i, column = 1, sticky = 'E',pady=2)
             self.entries.append(self.entr)
 
 
@@ -92,9 +153,7 @@ class MainApp(tk.Tk):
             frame.grid(row=5,column=5,columnspan=10,rowspan=10,sticky='NSEW')
             frame.grid_rowconfigure(0,weight=1)
             frame.grid_columnconfigure(0,weight=1)
-            a= frame.grid_size()
-            b=frame.grid_location(6,4)
-            print(a,b)
+
 
         self.show_frame(StartPage)
 
@@ -128,69 +187,108 @@ class MedPage1(tk.Frame):
     def __init__(self,parent,controller):
         tk.Frame.__init__(self,parent)
 
+        tk.Frame.grid_rowconfigure(self,1, weight=0)
+        tk.Frame.grid_rowconfigure(self,2, weight=0)
+        tk.Frame.grid_columnconfigure(self,1, weight=2)
+        tk.Frame.grid_columnconfigure(self,2, weight=3)
+        tk.Frame.grid_columnconfigure(self,3, weight=5)
 
         label = tk.Label(self, text='Products 1', font = LARGE_FONT)
-        label.pack(pady=10,padx=10)
+        label.grid(column=1, columnspan = 3,pady=10,padx=10,sticky='n')
 
         Product = ['ProdName','PrinAtivo','Description','TeraClassID','ProdType']
-        Product2 = ['PF0p','PF12p','PF17p','PF17p_ALC',
-        'PF17p5','PF17p5_ALC','PF18p','PF18p_ALC','PF20p','PMC0p','PMC12p','PMC17p',
-        'PMC17p_ALC','PMC17p5','PMC17p5_ALC','PMC18p','PMC18p_ALC','PMC20p']
-        Product3 =['RestHosp','CAP','CONFAZ87','AnalRecur','ListaTribID',
+        Product2 = ['RestHosp','CAP','CONFAZ87','AnalRecur','ListaTribID',
         'Comerc2016','Tarja']
+        Product3 = ['EAN','LabID','CodGGREM','Registro']
 
-        a = StrInputBoxes(Product,1,1,self)
-        b = StrInputBoxes(Product2,2,1,self)
+        a = StrInputBoxes(Product,4,self)
+        b = IntInputBoxes(Product2,7,self)
+        c = IntInputBoxes(Product3,4,self)
 
-        a.pack()
-        b.pack()
+        a.grid(row=1,column=1,sticky='w')
+        b.grid(row=1,column=2,rowspan=2,sticky='w')
+        c.grid(row=2,column=1,sticky='w')
+        c.grid_rowconfigure(1, weight=4)
+        c.grid_rowconfigure(2, weight=1)
+
         button1 = ttk.Button(self, text="Back to Home",command=lambda: controller.show_frame(StartPage))
-        button1.pack()
-        button2 = ttk.Button(self, text="Next",command=lambda: controller.show_frame(MedPage2))
-        button2.pack()
+        button1.grid(row=4,column=1,columnspan = 2, ipadx = 2)
+        button2 = ttk.Button(self, text="MedPage1",command=lambda: controller.show_frame(MedPage1))
+        button2.grid(row=3,column=1,columnspan = 1,pady=10,sticky='e')
+        button3 = ttk.Button(self, text="MedPage2",command=lambda: controller.show_frame(MedPage2))
+        button3.grid(row=3,column=2,columnspan = 1,pady=10,sticky='w')
+        button4 = ttk.Button(self, text="MedPage3",command=lambda: controller.show_frame(MedPage3))
+        button4.grid(row=3,column=3,columnspan = 1,pady=10,sticky='w')
 
 class MedPage2(tk.Frame):
 
     def __init__(self,parent,controller):
         tk.Frame.__init__(self,parent)
 
+        tk.Frame.grid_rowconfigure(self,2, weight=0)
+        tk.Frame.grid_rowconfigure(self,3, weight=3)
+        tk.Frame.grid_columnconfigure(self,0, weight=1)
+        tk.Frame.grid_columnconfigure(self,1, weight=3)
+        tk.Frame.grid_columnconfigure(self,2, weight=1)
 
         label = tk.Label(self, text='Products 2', font = LARGE_FONT)
-        label.pack(pady=10,padx=10)
+        label.grid(row=0,column=0,columnspan=2,pady=10,padx=10,sticky='n')
 
-        Product = ['EAN','ProdName','LabID','PrinAtivo','CodGGREM','Registro',
-        'Description','TeraClassID','ProdType']
-        Product2 = ['PF0p','PF12p','PF17p','PF17p_ALC',
-        'PF17p5','PF17p5_ALC','PF18p','PF18p_ALC','PF20p','PMC0p','PMC12p','PMC17p',
-        'PMC17p_ALC','PMC17p5','PMC17p5_ALC','PMC18p','PMC18p_ALC','PMC20p']
-        Product3 =['RestHosp','CAP','CONFAZ87','AnalRecur','ListaTribID',
-        'Comerc2016','Tarja']
+        Product = ['PF0p','PF12p','PF17p','PF17p_ALC']
+        Product2 = ['PF17p5','PF17p5_ALC','PF18p','PF18p_ALC']
+        Product3 = ['PF20p']
 
-        button1 = ttk.Button(self, text="Next",command=lambda: controller.show_frame(MedPage3))
-        button1.pack()
+        a = FloatInputBoxes(Product,4,self)
+        b = FloatInputBoxes(Product2,4,self)
+        c = FloatInputBoxes(Product3,1,self)
+        a.grid(row=1,column=0,columnspan=1,sticky='e',padx=10)
+        b.grid(row=1,column=1,columnspan=1,sticky='w')
+        c.grid(row=2,column=0,columnspan=2,sticky='n',pady=10)
+
+
+        button1 = ttk.Button(self, text="MedPage1",command=lambda: controller.show_frame(MedPage1))
+        button1.grid(row=3,column=0,columnspan = 1,pady=10,sticky='e')
+        button2 = ttk.Button(self, text="MedPage2",command=lambda: controller.show_frame(MedPage2))
+        button2.grid(row=3,column=1,columnspan = 1,pady=10,sticky='w')
+        button3 = ttk.Button(self, text="MedPage3",command=lambda: controller.show_frame(MedPage3))
+        button3.grid(row=3,column=2,columnspan =1,pady=10,sticky='w')
+        button3.grid_columnconfigure(3, weight=1)
+        button3.grid_columnconfigure(2, weight=5)
 
 class MedPage3(tk.Frame):
 
     def __init__(self,parent,controller):
         tk.Frame.__init__(self,parent)
 
+        tk.Frame.grid_rowconfigure(self,2, weight=0)
+        tk.Frame.grid_rowconfigure(self,3, weight=3)
+        tk.Frame.grid_columnconfigure(self,0, weight=1)
+        tk.Frame.grid_columnconfigure(self,1, weight=3)
+        tk.Frame.grid_columnconfigure(self,2, weight=3)
+
 
         label = tk.Label(self, text='Products 3', font = LARGE_FONT)
-        label.pack(pady=10,padx=10)
+        label.grid(row=0,column=0,columnspan=2,pady=10,padx=10,sticky='n')
 
-        Product = ['EAN','ProdName','LabID','PrinAtivo','CodGGREM','Registro',
-        'Description','TeraClassID','ProdType']
-        Product2 = ['PF0p','PF12p','PF17p','PF17p_ALC',
-        'PF17p5','PF17p5_ALC','PF18p','PF18p_ALC','PF20p','PMC0p','PMC12p','PMC17p',
-        'PMC17p_ALC','PMC17p5','PMC17p5_ALC','PMC18p','PMC18p_ALC','PMC20p']
-        Product3 =['RestHosp','CAP','CONFAZ87','AnalRecur','ListaTribID',
-        'Comerc2016','Tarja']
+        Product = ['PMC0p','PMC12p','PMC17p','PMC17p_ALC']
+        Product2 = ['PMC17p5','PMC17p5_ALC','PMC18p','PMC18p_ALC']
+        Product3 = ['PMC20p']
+
+        a = FloatInputBoxes(Product,4,self)
+        b = FloatInputBoxes(Product2,4,self)
+        c = FloatInputBoxes(Product3,1,self)
+        a.grid(row=1,column=0,columnspan=1,sticky='e',padx=10)
+        b.grid(row=1,column=1,columnspan=1,sticky='w')
+        c.grid(row=2,column=0,columnspan=2,sticky='n',pady=10)
 
 
 
-
-        button1 = ttk.Button(self, text="Back to Home",command=lambda: controller.show_frame(StartPage))
-        button1.pack()
+        button1 = ttk.Button(self, text="MedPage1",command=lambda: controller.show_frame(MedPage1))
+        button1.grid(row=3,column=0,columnspan = 1,pady=10,sticky='e')
+        button2 = ttk.Button(self, text="MedPage2",command=lambda: controller.show_frame(MedPage2))
+        button2.grid(row=3,column=1,columnspan = 1,pady=10,sticky='w')
+        button3 = ttk.Button(self, text="MedPage3",command=lambda: controller.show_frame(MedPage3))
+        button3.grid(row=3,column=2,columnspan = 1,pady=10,sticky='w')
 
 
 class PageTwo(tk.Frame):
@@ -204,6 +302,8 @@ class PageTwo(tk.Frame):
         LabName =['LabName','LabCNPJ','LabOfficeAddress','LabContactEmail1',
         'LabContactEmail2', 'LabContactPhone1', 'LabContactPhone2',
         'LabContactFax']
+        a = StrInputBoxes(LabName,8,self)
+        a.pack()
 
 
         button1 = ttk.Button(self, text="Back to Home",command=lambda: controller.show_frame(StartPage))
@@ -220,7 +320,8 @@ class PageThree(tk.Frame):
         ProviderComp = ['ProvCompName','ProvCompCNPJ','ProvOfficeAddress',
         'ProvContactEmail1','ProvContactEmail2','ProvContactPhone1','ProvContactPhone2',
         'ProvContactFax']
-
+        a = StrInputBoxes(ProviderComp,8,self)
+        a.pack()
 
         button1 = ttk.Button(self, text="Back to Home",command=lambda: controller.show_frame(StartPage))
         button1.pack()
@@ -239,66 +340,8 @@ app.mainloop()
 
 
 
-class IntInputBoxes(tk.Frame):
-
-    def __init__(self,InputNames,parent):
-        tk.Frame.__init__(self,parent)
-        self.InputNames = InputNames
-        self.EntryNumbers = len(self.InputNames)
-        self.entries = []
-
-        self.create_entry_widgets(self.EntryNumbers,self.InputNames,
-        self.entries)
-
-    def create_entry_widgets(self,EntryNumbers,InputNames,entries):
-
-        self.ctrl = int(EntryNumbers)
-        self.List = InputNames
-
-        for i in range(self.ctrl):
-
-            self.text = tk.Label(self,text = self.List[i],)
-
-            self.text.grid(row=i, column = 1, sticky = 'E',pady=2)
-            self.text.grid_columnconfigure(1,weight=1)
-
-        for i in range(self.ctrl):
-
-            self.entr = tk.Entry(self,bg='azure',textvariable = tk.IntVar())
-            self.entr.grid(row=i, column = 2, sticky = 'W',pady=2)
-            self.entries.append(self.entr)
 
 
-
-
-class FloatInputBoxes(tk.Frame):
-
-    def __init__(self,InputNames,parent):
-        tk.Frame.__init__(self,parent)
-        self.InputNames = InputNames
-        self.EntryNumbers = len(self.InputNames)
-        self.entries = []
-
-        self.create_entry_widgets(self.EntryNumbers,self.InputNames,
-        self.entries)
-
-    def create_entry_widgets(self,EntryNumbers,InputNames,entries):
-
-        self.ctrl = int(EntryNumbers)
-        self.List = InputNames
-
-        for i in range(self.ctrl):
-
-            self.text = tk.Label(self,text = self.List[i],)
-
-            self.text.grid(row=i, column = 1, sticky = 'E',pady=2)
-            self.text.grid_columnconfigure(1,weight=1)
-
-        for i in range(self.ctrl):
-
-            self.entr = tk.Entry(self,bg='azure',textvariable = tk.DoubleVar())
-            self.entr.grid(row=i, column = 2, sticky = 'W',pady=2)
-            self.entries.append(self.entr)
 
 
 
