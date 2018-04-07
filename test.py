eb2a80aa3064bc220ab1006c7c6df15790f0f774
FTAlves <ftalves@poli.ufrj.br>
2018-04-07 03:14:08 -0300

    Clean master

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ import os
 from src.setup.HighCommands import *
 from src.inputNFe.readNFe import *
 from src.inputNFe.inputNFe2DB import *
-from src.GUI import MainApp as MaP
 
 os.makedirs('data',exist_ok=True)
 db = sqlite3.connect('data/testdb.db')
@@ -15,8 +14,3 @@ filename = 'data/xml_test.xml'
 NFeDict = readNFe(filename)
 
 inputNFe2DB(NFeDict,db)
-
-app = MaP.MainApp()
-app.geometry("940x270")
-app.resizable(width=True, height=True)
-app.mainloop()
